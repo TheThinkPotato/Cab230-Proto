8e8f5149c298412d533aaad8aa47058f559eff4d
Dan Lopez <86757951+TheThinkPotato@users.noreply.github.com>
2022-05-03 21:54:01 +1000

    :lipstick: Logo now links back to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Nav from "./Nav";
+import { useNavigate } from "react-router-dom";
 
 // the header
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <header>
       <div style={{ display: "flex" }}>
-        <div className="icon">
-          <img src="images/logo.png" alt="Logo" />
+        <div className="icon">          
+          <img src="images/logo.png" alt="Logo"  onClick={()=> navigate("/") }/>          
         </div>
 
         <Nav />
