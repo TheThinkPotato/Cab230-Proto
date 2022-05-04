4632c0ba68732efd7d712409360dec4aa61d35ea
Dan Lopez <86757951+TheThinkPotato@users.noreply.github.com>
2022-05-04 20:38:34 +1000

    :recycle: refactor home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button } from "reactstrap";
 import Header from "../components/Header";
 
